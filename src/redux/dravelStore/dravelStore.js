563da35661e449ec42141904b5e911db56e77234
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-07 00:21:40 +0100

    Login Logic

--- src/redux/dravelStore/dravelStore.js
+++ src/redux/dravelStore/dravelStore.js
@@ -1,7 +1,16 @@
-const initialState = [];
+const Login_USER = 'Login_USER';
+const initialState = { user: '' };
+
+export const loginUser = (payload) => ({
+  type: Login_USER,
+  payload,
+});
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case Login_USER:
+      return { ...state, user:action.payload};
+
     default:
       return state;
   }
