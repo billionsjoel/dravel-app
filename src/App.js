ef127842b8a38e667f99e3034abf25f45039ad71
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-06 23:43:59 +0100

    Add route for the splash page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+} from 'react-router-dom';
 import Splash from './pages/splash';
 
 function App() {
   return (
     <div className="App">
-      <Splash />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Splash />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
