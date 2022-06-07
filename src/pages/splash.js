52be7c33a3283c8015c0c68016cc4deda193cb7b
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-07 18:18:54 +0300

    Add bootstrap

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -3,7 +3,7 @@ import Login from '../components/splash/login';
 function Splash() {
   return (
     <>
-      <div>
+      <div className="splash">
         <p> This is the splash page </p>
         <Login />
       </div>
