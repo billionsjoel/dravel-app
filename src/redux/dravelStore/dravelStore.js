e108065ae6b525d1a736c3a3390c164ed8135272
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-07 08:45:28 +0100

    fix linters

--- src/redux/dravelStore/dravelStore.js
+++ src/redux/dravelStore/dravelStore.js
@@ -1,15 +1,15 @@
-const Login_USER = 'Login_USER';
+const LoginUSER = 'Login_USER';
 const initialState = { user: '' };
 
 export const loginUser = (payload) => ({
-  type: Login_USER,
+  type: LoginUSER,
   payload,
 });
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case Login_USER:
-      return { ...state, user:action.payload};
+    case LoginUSER:
+      return { ...state, user: action.payload };
 
     default:
       return state;
