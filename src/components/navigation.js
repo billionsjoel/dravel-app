3063bb26cdce786b597e7d60f1807a1d4988a08f
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 18:11:10 +0300

    Fix linter

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,29 +1,29 @@
 import '../scss/nav.scss';
 
 function Navigation() {
-	return (
-		<div className="menu-container">
-			<div className="logo">Dravel</div>
-			<ul className="menu">
-				<li>Destinations</li>
-				<li>Trips</li>
-				<li>Reserve</li>
-			</ul>
-			<div className="socials fixed-bottom">
-				<ul>
-					<li>
-						<i class="fa fa-twitter" aria-hidden="true"></i>
-					</li>
-					<li>
-						<i class="fa fa-facebook" aria-hidden="true"></i>
-					</li>
-					<li>
-						<i class="fa fa-youtube-play" aria-hidden="true"></i>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className="menu-container">
+      <div className="logo">Dravel</div>
+      <ul className="menu">
+        <li>Destinations</li>
+        <li>Trips</li>
+        <li>Reserve</li>
+      </ul>
+      <div className="socials fixed-bottom">
+        <ul>
+          <li>
+            <i className="fa fa-twitter" aria-hidden="true" />
+          </li>
+          <li>
+            <i className="fa fa-facebook" aria-hidden="true" />
+          </li>
+          <li>
+            <i className="fa fa-youtube-play" aria-hidden="true" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
