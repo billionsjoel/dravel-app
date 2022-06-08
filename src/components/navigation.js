b2d7d9ba6d216fb965e643feae4227c2c6fe6a95
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 18:32:16 +0300

    Fix linter

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,33 +1,33 @@
 import '../scss/nav.scss';
 
 function Navigation() {
-	return (
-		<div className="menu-container">
-			<div className="logo">Dravel</div>
-			<ul className="menu">
-				<li>Destinations</li>
-				<li>Trips</li>
-				<li>Reserve</li>
-			</ul>
-			<div className="socials fixed-bottom">
-				<ul>
-					<li>
-						<i className="fa fa-twitter" aria-hidden="true" />
-					</li>
-					<li>
-						<i className="fa fa-facebook" aria-hidden="true" />
-					</li>
-					<li>
-						<i className="fa fa-youtube-play" aria-hidden="true" />
-					</li>
-					<li>
-						<i className="fa fa-google-plus" aria-hidden="true" />
-					</li>
-				</ul>
-				<span>&copy; copyright 2022</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className="menu-container">
+      <div className="logo">Dravel</div>
+      <ul className="menu">
+        <li>Destinations</li>
+        <li>Trips</li>
+        <li>Reserve</li>
+      </ul>
+      <div className="socials fixed-bottom">
+        <ul>
+          <li>
+            <i className="fa fa-twitter" aria-hidden="true" />
+          </li>
+          <li>
+            <i className="fa fa-facebook" aria-hidden="true" />
+          </li>
+          <li>
+            <i className="fa fa-youtube-play" aria-hidden="true" />
+          </li>
+          <li>
+            <i className="fa fa-google-plus" aria-hidden="true" />
+          </li>
+        </ul>
+        <span>&copy; copyright 2022</span>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
