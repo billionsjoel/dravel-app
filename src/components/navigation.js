bc1565273430c82b080f8406dbfd310093780271
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 16:46:30 +0300

    Add style to menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,8 +2,9 @@ import '../scss/nav.scss';
 
 function Navigation() {
   return (
-    <div className="menu-items">
-      <ul>
+    <div className="menu-container">
+      <div className="logo">Dravel</div>
+      <ul className="menu">
         <li>Destinations</li>
         <li>Trips</li>
         <li>Reserve</li>
