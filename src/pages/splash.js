89d68426ad5607bb395c80d84736172cc10dfe3d
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 12:23:59 +0300

    Add content

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -2,16 +2,27 @@
 import Login from '../components/splash/login';
 
 function Splash() {
-	return (
-		<>
-			<div className="cover">
-				<div className="nav">
-					<div className="logo">Dravel</div>
-					<div className="search">icon</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="cover">
+        <div className="nav">
+          <div className="logo">
+            <i className="fa-solid fa-bars" />
+            Dravel
+          </div>
+          <div className="search">
+            <i className="fa fa-search" aria-hidden="true" />
+          </div>
+        </div>
+        <div className="content">
+          <Login />
+        </div>
+        <div className="footer">
+          &copy; Copyright Dravel 2022
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Splash;
