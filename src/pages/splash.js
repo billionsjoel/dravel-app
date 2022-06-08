89284bd97cc86aa307dd4503b345011694ef38fe
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 12:11:23 +0300

    Install font awesome

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -2,54 +2,16 @@
 import Login from '../components/splash/login';
 
 function Splash() {
-  return (
-    <>
-      <div className="text-center">
-        <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
-          <header className="masthead mb-auto">
-            <div className="inner">
-              <h3 className="masthead-brand">Cover</h3>
-              <nav className="nav nav-masthead justify-content-center">
-                <a className="nav-link active" href="#">
-                  Home
-                </a>
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-                <a className="nav-link" href="#">
-                  Contact
-                </a>
-              </nav>
-            </div>
-          </header>
-
-          <main role="main" className="inner cover">
-            <h1 className="cover-heading">Dravel</h1>
-            <p className="lead">Discover while you Travel.</p>
-            <p className="lead">
-              <a href="#" className="btn btn-md btn-block btn-warning">
-                <Login />
-              </a>
-            </p>
-          </main>
-
-          <footer className="mastfoot mt-auto">
-            <div className="inner">
-              <p>
-                Copyright &copy;
-                {' '}
-                <a href="https://getbootstrap.com/">Dravel</a>
-                , by
-                {' '}
-                <a href="https://twitter.com/mdo">@dravel</a>
-                .
-              </p>
-            </div>
-          </footer>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="cover">
+				<div className="nav">
+					<div className="logo">Dravel</div>
+					<div className="search">icon</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Splash;
