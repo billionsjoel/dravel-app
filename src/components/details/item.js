70d83d6ebd95c8948a010f72a7bec0624d2ce1b1
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 22:25:19 +0300

    Add image to item

--- src/components/details/item.js
+++ src/components/details/item.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function Item() {
   return (
-    <div>
-      item
+    <div className="image">
+      <img
+        src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80"
+        alt=""
+        height={500}
+        width={500}
+      />
     </div>
   );
 }
--- src/components/homepage/itemsContainer.js
+++ src/components/homepage/itemsContainer.js
@@ -1,3 +1,5 @@
+import Item from '../details/item';
+
 function ItemsConainer() {
   return (
     <>
@@ -8,12 +10,7 @@ function ItemsConainer() {
         </div>
         <div className="items">
           <div className="images">
-            <img
-              src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80"
-              alt=""
-              height={500}
-              width={500}
-            />
+            <Item />
           </div>
         </div>
       </div>
