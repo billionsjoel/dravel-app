612df2f7403cc82c1e6d7f1986bc66804844233a
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 18:22:06 +0300

    Add g+

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,29 +1,32 @@
 import '../scss/nav.scss';
 
 function Navigation() {
-  return (
-    <div className="menu-container">
-      <div className="logo">Dravel</div>
-      <ul className="menu">
-        <li>Destinations</li>
-        <li>Trips</li>
-        <li>Reserve</li>
-      </ul>
-      <div className="socials fixed-bottom">
-        <ul>
-          <li>
-            <i className="fa fa-twitter" aria-hidden="true" />
-          </li>
-          <li>
-            <i className="fa fa-facebook" aria-hidden="true" />
-          </li>
-          <li>
-            <i className="fa fa-youtube-play" aria-hidden="true" />
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="menu-container">
+			<div className="logo">Dravel</div>
+			<ul className="menu">
+				<li>Destinations</li>
+				<li>Trips</li>
+				<li>Reserve</li>
+			</ul>
+			<div className="socials fixed-bottom">
+				<ul>
+					<li>
+						<i className="fa fa-twitter" aria-hidden="true" />
+					</li>
+					<li>
+						<i className="fa fa-facebook" aria-hidden="true" />
+					</li>
+					<li>
+						<i className="fa fa-youtube-play" aria-hidden="true" />
+					</li>
+					<li>
+						<i className="fa fa-google-plus" aria-hidden="true" />
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default Navigation;
