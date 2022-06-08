5a6ce2a5e7d2b06e5dc5468875d11311a6b7c1c1
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-08 18:10:56 +0300

    Add socials

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,29 @@
 import '../scss/nav.scss';
 
 function Navigation() {
-  return (
-    <div className="menu-container">
-      <div className="logo">Dravel</div>
-      <ul className="menu">
-        <li>Destinations</li>
-        <li>Trips</li>
-        <li>Reserve</li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="menu-container">
+			<div className="logo">Dravel</div>
+			<ul className="menu">
+				<li>Destinations</li>
+				<li>Trips</li>
+				<li>Reserve</li>
+			</ul>
+			<div className="socials fixed-bottom">
+				<ul>
+					<li>
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+					</li>
+					<li>
+						<i class="fa fa-facebook" aria-hidden="true"></i>
+					</li>
+					<li>
+						<i class="fa fa-youtube-play" aria-hidden="true"></i>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default Navigation;
