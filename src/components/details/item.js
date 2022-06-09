380612a42200d9dbb1313fa2b09faf4fa3ebf902
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-09 11:44:02 +0300

    Add style to item

--- src/components/details/item.js
+++ src/components/details/item.js
@@ -3,7 +3,7 @@ import '../../scss/item.scss';
 
 function Item() {
   return (
-    <div className="item-container">
+    <div className="image">
       <img
         src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80"
         alt=""
