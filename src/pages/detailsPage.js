df81d6c689da93a96c1e3506eb7956d55221f78f
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-09 15:49:41 +0300

    Add details image

--- src/pages/detailsPage.js
+++ src/pages/detailsPage.js
@@ -1,4 +1,5 @@
 import Navigation from '../components/navigation';
+import '../scss/details.scss';
 
 function DetailsPage() {
   return (
@@ -9,6 +10,7 @@ function DetailsPage() {
         </div>
         <div className="image">
           <img
+            className="details-img"
             src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
             alt=""
             height={500}
