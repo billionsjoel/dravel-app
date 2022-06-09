dca67dff50a5cdb92cb52380752be73f383829cc
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-09 15:33:43 +0300

    Add details

--- src/pages/detailsPage.js
+++ src/pages/detailsPage.js
@@ -4,7 +4,16 @@ function DetailsPage() {
   return (
     <>
       <div className="details">
-        <Navigation />
+        <div className="menu">
+          <Navigation />
+        </div>
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+            alt=""
+          />
+        </div>
+        <div className="booking-section"> Booking details </div>
       </div>
     </>
   );
