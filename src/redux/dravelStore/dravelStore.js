c2f848fc2c038fbb13820d04737cdac4d7d901cc
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-09 12:41:29 +0100

    Add dummy data for reservation

--- src/redux/dravelStore/dravelStore.js
+++ src/redux/dravelStore/dravelStore.js
@@ -9,6 +9,11 @@ const initialState = {
   }, {
     name: 'item 2',
   }],
+  reservation: [{
+    itemName: 'item 1',
+    date: '2020-20-13',
+    city: 'alger',
+  }],
 };
 
 export const loginUser = (payload) => ({
@@ -43,7 +48,7 @@ const reducer = (state = initialState, action) => {
         return {...x};
       });
       return { ...state, items: newItemsState };
-      
+
     default:
       return state;
   }
