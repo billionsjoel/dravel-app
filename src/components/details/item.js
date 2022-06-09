b509c957a88380e0f5407a89d9aafed24bc00069
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-09 12:23:09 +0300

    Style items

--- src/components/details/item.js
+++ src/components/details/item.js
@@ -8,8 +8,8 @@ function Item() {
         <img
           src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80"
           alt=""
-          height={200}
-          width={200}
+          height={500}
+          width={500}
         />
         <div className="image-title">Discovery One</div>
         <div className="description">
