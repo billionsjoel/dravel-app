d4cea089b61481776e044b44b741bda1385ce6bb
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-09 12:32:25 +0100

    Delete Logic

--- src/pages/deletePage.js
+++ src/pages/deletePage.js
@@ -1,11 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteItem } from '../redux/dravelStore/dravelStore';
 import Navigation from '../components/navigation';
 
 const Deletepage = () => {
-  console.log('hi');
+  const items = useSelector((state) => state.dravelReducer.items);
+  const dispatch = useDispatch();
+  const handleDelete = (e) => {
+    dispatch(deleteItem(e.target.value));
+  };
   return (
     <div>
       <h2>delete page</h2>
       <Navigation />
+      {
+      /* eslint-disable */
+        items.map((item,index) => (
+          <div key={index}>
+            <p>{item.name}</p>
+            <button onClick={handleDelete} value={item.name}>delete</button>
+          </div>
+        ))
+      }
     </div>
   );
 };
