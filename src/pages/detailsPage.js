0d8894f08f379e1664b2759c820f6a1f5c855e74
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-09 15:34:21 +0300

    Add image size

--- src/pages/detailsPage.js
+++ src/pages/detailsPage.js
@@ -11,6 +11,8 @@ function DetailsPage() {
           <img
             src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
             alt=""
+            height={500}
+            width={500}
           />
         </div>
         <div className="booking-section"> Booking details </div>
