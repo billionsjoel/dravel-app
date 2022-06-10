b9d22669c6b7e308cfc00d8e455fa2ae3b0d0cf0
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-10 17:23:33 +0300

    Create container and scss file

--- src/pages/reservation.js
+++ src/pages/reservation.js
@@ -1,26 +1,28 @@
-import '../../src/scss/reservation.scss';
+import '../scss/reservation.scss';
 
 function ReservationPage() {
   return (
     <>
-      <div className="r-title">
-        <h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
-      </div>
-      <div className="r-description">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
-          dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
-          Reiciendis, eligendi!
-        </p>
-      </div>
-      <div className="r-cta">
-        <select name="" id="">
-          <option value="">Kampala</option>
-        </select>
-        <button type="button">
-          <div className="btn">Reserve Now</div>
-        </button>
+      <div className="r-container">
+        <div className="r-title">
+          <h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
+        </div>
+        <div className="r-description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
+            dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
+            Reiciendis, eligendi!
+          </p>
+        </div>
+        <div className="r-cta">
+          <select name="" id="">
+            <option value="">Kampala</option>
+          </select>
+          <button type="button">
+            <div className="btn">Reserve Now</div>
+          </button>
+        </div>
       </div>
     </>
   );
