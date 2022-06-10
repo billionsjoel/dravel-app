c4d6cbcd93dbe2700c7bf96fc10f788e69caa2a9
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-10 17:20:47 +0300

    Import reservation scss

--- src/pages/reservation.js
+++ src/pages/reservation.js
@@ -1,27 +1,29 @@
+import '../../src/scss/reservation.scss';
+
 function ReservationPage() {
-	return (
-		<>
-			<div className="r-title">
-				<h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
-			</div>
-			<div className="r-description">
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-					reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
-					dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
-					Reiciendis, eligendi!
-				</p>
-			</div>
-			<div className="r-cta">
-				<select name="" id="">
-					<option value="">Kampala</option>
-				</select>
-				<button>
-					<div className="btn">Reserve Now</div>
-				</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="r-title">
+        <h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
+      </div>
+      <div className="r-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+          reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
+          dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
+          Reiciendis, eligendi!
+        </p>
+      </div>
+      <div className="r-cta">
+        <select name="" id="">
+          <option value="">Kampala</option>
+        </select>
+        <button type="button">
+          <div className="btn">Reserve Now</div>
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default ReservationPage;
