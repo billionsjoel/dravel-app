05cc7b626e69b7b782385dd1bcc43edb135a3a3a
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-10 17:10:26 +0300

    Fix linter

--- src/pages/reservation.js
+++ src/pages/reservation.js
@@ -1,19 +1,19 @@
 function ReservationPage() {
-	return (
-		<>
-			<div className="r-title">
-				<h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
-			</div>
-			<div className="r-description">
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-					reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
-					dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
-					Reiciendis, eligendi!
-				</p>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="r-title">
+        <h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
+      </div>
+      <div className="r-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+          reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
+          dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
+          Reiciendis, eligendi!
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default ReservationPage;
