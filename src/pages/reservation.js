21e069a0ec596719091c0ffbd7cede3d06583437
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-10 17:13:08 +0300

    Add button

--- src/pages/reservation.js
+++ src/pages/reservation.js
@@ -1,24 +1,27 @@
 function ReservationPage() {
-  return (
-    <>
-      <div className="r-title">
-        <h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
-      </div>
-      <div className="r-description">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
-          dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
-          Reiciendis, eligendi!
-        </p>
-      </div>
-      <div className="r-cta">
-        <select name="" id="">
-          <option value="">Kampala</option>
-        </select>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="r-title">
+				<h3>RESERVE A DRAVEL TEST-JOURNEY</h3>
+			</div>
+			<div className="r-description">
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+					reprehenderit maxime? Placeat a iste explicabo maiores! Nisi fugit
+					dolorem neque explicabo vitae. Minima minus doloremque sunt sit sed!
+					Reiciendis, eligendi!
+				</p>
+			</div>
+			<div className="r-cta">
+				<select name="" id="">
+					<option value="">Kampala</option>
+				</select>
+				<button>
+					<div className="btn">Reserve Now</div>
+				</button>
+			</div>
+		</>
+	);
 }
 
 export default ReservationPage;
