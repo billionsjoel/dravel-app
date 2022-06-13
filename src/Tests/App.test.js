168c227b68d751184397784feed3f0a4c3b94b0e
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-13 19:32:20 +0300

    Add tests to app test refactor

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import {
+  render, cleanup, within, screen,
+} from '@testing-library/react';
 import App from '../App';
+import '@testing-library/jest-dom';
+import Splash from '../pages/splash';
+import HomePage from '../pages/homePage';
+import DetailsPage from '../pages/detailsPage';
+import DeletePage from '../pages/deletePage';
+import ReservationPage from '../pages/reservation';
 
 afterEach(cleanup);
 
 describe('It should test UI DOM components', () => {
   it('should take a snapshot', () => {
     const { asFragment } = render(<App />);
-
     expect(asFragment(<App />)).toMatchSnapshot();
+  });
+  it('should render the title on the splash page', () => {
+    render(<Splash />);
+    const { getByText } = within(screen.getByTestId('app-title'));
+    expect(getByText('Dravel')).toBeInTheDocument();
+  });
+  it('should render the title on the home page', () => {
+    render(<HomePage />);
+    const { getByText } = within(screen.getByTestId('app-title'));
+    expect(getByText('LATEST DISCOVERIES')).toBeInTheDocument();
+  });
+  it('should render the title on the details page', () => {
+    render(<DetailsPage />);
+    const { getByText } = within(screen.getByTestId('app-title'));
+    expect(getByText('Product heading')).toBeInTheDocument();
+  });
+  it('should render the title on the delete page', () => {
+    render(<DeletePage />);
+    const { getByText } = within(screen.getByTestId('app-title'));
+    expect(getByText('Delete')).toBeInTheDocument();
+  });
+  it('should render the title on the reservation page', () => {
+    render(<ReservationPage />);
+    const { getByText } = within(screen.getByTestId('app-title'));
+    expect(getByText('RESERVE A DRAVEL')).toBeInTheDocument();
   });
 });
