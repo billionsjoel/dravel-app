ca6b87a5b040b49bd2c5e2a30aaeeebada3ecaaf
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-13 14:51:11 +0100

    Add item logic

--- src/pages/addPage.js
+++ src/pages/addPage.js
@@ -1,7 +1,35 @@
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../redux/dravelStore/dravelStore';
+
 function AddPage() {
+  const item = useRef();
+  const des = useRef();
+  const image = useRef();
+  const dispatch = useDispatch();
+  const handleAdd = (e) => {
+    e.preventDefault();
+    dispatch(addItem({
+      name: item.current.value,
+      img: image.current.value,
+      des: des.current.value,
+    }));
+    item.current.value = '';
+    image.current.value = '';
+    des.current.value = '';
+  };
   return (
     <div>
-      add page
+      <h2>addPage</h2>
+      <form onSubmit={handleAdd}>
+        <input type="text" ref={item} placeholder="item name" />
+        <input type="text" ref={des} placeholder="des" />
+        <input type="text" ref={image} placeholder="img_url" />
+        <button className="cta" type="submit">
+          <span className="fa fa-cog p-2" aria-hidden="true" />
+          Add item
+        </button>
+      </form>
     </div>
   );
 }
--- src/redux/dravelStore/dravelStore.js
+++ src/redux/dravelStore/dravelStore.js
@@ -1,4 +1,6 @@
-const LoginUSER = 'Login_USER';
+const LOGINUSER = 'LOGIN_USER';
+const ADDITEM = 'ADD_ITEM';
+
 const initialState = {
   user: '',
   items: [{
@@ -13,14 +15,22 @@ const initialState = {
 };
 
 export const loginUser = (payload) => ({
-  type: LoginUSER,
+  type: LOGINUSER,
+  payload,
+});
+
+export const addItem = (payload) => ({
+  type: ADDITEM,
   payload,
 });
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LoginUSER:
+    case LOGINUSER:
       return { ...state, user: action.payload };
+
+    case ADDITEM:
+      return { ...state, items: [...state.items, action.payload] };
 
     default:
       return state;
