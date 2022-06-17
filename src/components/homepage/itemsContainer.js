1e7dbdfcdf55cc4218343de689d66811af52dbbd
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-06-17 19:58:55 +0300

    Add more styles

--- src/components/homepage/itemsContainer.js
+++ src/components/homepage/itemsContainer.js
@@ -8,45 +8,49 @@ import Item from '../details/item';
 import '../../scss/items.scss';
 /* eslint-disable */
 function ItemsConainer() {
-  const dispatch = useDispatch();
-  const items = useSelector((state) => state.dravelReducer.items);
-  const token = useSelector((state) => state.dravelReducer.user);
-  const reload = useSelector((state) => state.dravelReducer.user);
-  useEffect(() => {
-    axios.get('https://dravel-api.herokuapp.com/trips')
-    .then(function (response) {
-      if (response.status === 200) {
-        console.log(response.data);
-        dispatch(fetchItems(response.data));
-      }
-
-    }).catch(function (error) {
-      console.log(error)
-    });
-
-  }, []);
-  return (
-    <>
-      <div className="items-container">
-        <div className="items-heading">
-          <h1>LATEST DISCOVERIES</h1>
-          <p>Please select a discovery</p>
-        </div>
-        <div className="items">
-          <div className="images">
-            {
-            /* eslint-disable */
-              items.map((item) => (
-                <Link key={item.id} to="/details" state={{ item }}>
-                  <Item item={item}/>
-                </Link>
-              ))
-            }
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	const dispatch = useDispatch();
+	const items = useSelector((state) => state.dravelReducer.items);
+	const token = useSelector((state) => state.dravelReducer.user);
+	const reload = useSelector((state) => state.dravelReducer.user);
+	useEffect(() => {
+		axios
+			.get('https://dravel-api.herokuapp.com/trips')
+			.then(function (response) {
+				if (response.status === 200) {
+					console.log(response.data);
+					dispatch(fetchItems(response.data));
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
+	return (
+		<>
+			<div className="items-container">
+				<div className="items-heading">
+					<h1>LATEST DISCOVERIES</h1>
+					<p>Please select a discovery</p>
+				</div>
+				<div className="items">
+					<div className="images">
+						<div className="row">
+							{
+								/* eslint-disable */
+								items.map((item) => (
+									<div className="col-md-4 col-sm-6">
+										<Link key={item.id} to="/details" state={{ item }}>
+											<Item item={item} />
+										</Link>
+									</div>
+								))
+							}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default ItemsConainer;
