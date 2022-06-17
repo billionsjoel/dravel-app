eff3068059afa8dcc3c9b300a62b4674eb789b61
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-06-17 15:27:25 +0100

    Update reserve.js

--- src/pages/reserve.js
+++ src/pages/reserve.js
@@ -45,7 +45,6 @@ const Reserve = () => {
         <div className="reserve-content">
           <h1 className="p-4 text-center">Reserve now</h1>
           <form onSubmit={handleAdd} className="form form-control">
-            <input type="text" ref={city} className="form-control" />
             <input type="date" ref={date} className="form-control" />
             <select value={item} onChange={(e) => setItem(e.target.value)} className="form-control">
               {
